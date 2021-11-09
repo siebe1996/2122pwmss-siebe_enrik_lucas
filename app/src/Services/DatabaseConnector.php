fcae31267a5f564bba56c7a65fe2943e80e3e4ea
Siebe Van de Voorde <siebe.vandevoorde@student.odisee.be>
2021-11-09 10:07:58 +0100

    adding project tree

---
+++ app/src/Services/DatabaseConnector.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Services;
+
+use SplFileObject;
+
+class DatabaseConnector
+{
+    static function getConnection() : \Doctrine\DBAL\Connection {
+        $connectionParams = [
+            'url' => 'mysql://' . DB_USER . ':' . DB_PASS . '@' . DB_HOST . '/' . DB_NAME
+        ];
+
+        try {
+            $connection = \Doctrine\DBAL\DriverManager::getConnection($connectionParams);
+            $connection->connect();
+        } catch (\Doctrine\DBAL\Exception $e) {
+            if (DEBUG){
+                echo($e->getMessage() . PHP_EOL);
+                exit();
+            }
+            else{
+                $filename = __DIR__.'/../../storage/db.log';
+                $file = new \SplFileObject($filename,'r');
+                $file->fwrite((new \DateTime()) -> format(\DateTimeInterface::RSS).'-'.$e->getMessage() . PHP_EOL);
+
+                header('Location: /unavailable.html');
+                exit();
+            }
+        }
+        return $connection;
+    }
+
+}
