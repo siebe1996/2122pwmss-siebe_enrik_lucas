eae8f0331534aab93396762821ab6f545f452059
enrik <enrik.paepe@student.odisee.be>
2021-11-27 11:35:24 +0100

    Moved twig files to pages folder

--- app/public/index.php
+++ app/public/index.php
@@ -3,12 +3,12 @@ require_once ('../vendor/autoload.php');
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../resources/templates');
 $twig = new Twig\Environment($loader, [
-    //'cache' => __DIR__ . '/../../storage/cache',
+    'cache' => __DIR__ . '/../../storage/cache',
     'auto-reload' => true
 ]);
 $variables = [
 
 ];
 
-$tpl = $twig->load('index.twig');
+$tpl = $twig->load('pages/index.twig');
 echo $tpl->render($variables);
