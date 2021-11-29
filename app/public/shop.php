95fd380c594f73945f7c1d63873b5ffe33eb251c
enrik <enrik.paepe@student.odisee.be>
2021-11-30 00:08:32 +0100

    Changed shop.php and shop.twig. It works now, the sort doesn't yet.

--- app/public/shop.php
+++ app/public/shop.php
@@ -5,7 +5,7 @@ require_once ('../src/Services/DatabaseConnector.php');
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../resources/templates');
 $twig = new Twig\Environment($loader, [
-    'cache' => __DIR__ . '/../storage/cache',
+    //'cache' => __DIR__ . '/../storage/cache',
     'auto-reload' => true
 ]);
 
@@ -17,6 +17,11 @@ $sort = isset($_POST['sort']) ? (string)$_POST['sort'] : '';
 
 $query = 'SELECT * FROM products';
 
+$category = isset($_GET['category']) ? (string)$_GET['category'] : '';
+
+
+$productId = isset($_GET['id']) ? (string)$_GET['id'] : '';
+
 if(strtoupper($sort) == 'ASC') {
     $query = 'SELECT * FROM products ORDER BY price ASC';
 }
@@ -27,14 +32,42 @@ else if(strtoupper($sort) == 'POPULARITY') {
     $query = 'SELECT * FROM products ORDER BY stock';
 }
 
+if(trim($productId)!='') {
+    $query = 'SELECT * FROM products WHERE id = ' . $productId;
+}
+
+if(trim($category)!='') {
+    $query = 'SELECT * FROM products WHERE categories_id = ' . $category;
+    if(strtoupper($sort) == 'ASC') {
+        $query = 'SELECT * FROM products WHERE categories_id = ' . $category . ' ORDER BY price ASC';
+    }
+    else if(strtoupper($sort) == 'DESC') {
+        $query = 'SELECT * FROM products WHERE categories_id = ' . $category . ' ORDER BY price DESC';
+    }
+    else if(strtoupper($sort) == 'POPULARITY') {
+        $query = 'SELECT * FROM products WHERE categories_id = ' . $category . ' ORDER BY stock';
+    }
+}
+
+
+
 $stmt = $conn->prepare($query);
 $products = $stmt->executeQuery()->fetchAllAssociative();
 
+
+
+
 $amountOfResults = count($products);
+if($amountOfResults=='0') {
+    $amountOfResults = 'Op dit moment hebben we niets van deze product(en';
+}
 
 $query = 'SELECT * FROM categories';
 $stmt = $conn->prepare($query);
 $categories = $stmt->executeQuery()->fetchAllAssociative();
+
+
+
 
 
 $variables = [
@@ -45,4 +78,5 @@ $variables = [
 ];
 
 $tpl = $twig->load('pages/shop.twig');
-echo $tpl->render();
+echo $tpl->render($variables);
+
